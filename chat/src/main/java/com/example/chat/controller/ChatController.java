8e9791fc28b48e921339046bd7855bb21c7a36bb
ZeyuZuo <zeyuzuo916@gmail.com>
2024-04-22 15:36:43 +0800

    delete ChatController

--- chat/src/main/java/com/example/chat/controller/ChatController.java
+++ chat/src/main/java/com/example/chat/controller/ChatController.java
@@ -1,23 +1,23 @@
-package com.example.chat.controller;
-
-import com.example.chat.netty.NettyServer;
-import com.example.chat.netty.element.Content;
-import com.example.chat.netty.handler.ChatHandler;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping(value = "/chat", produces = "application/json;charset=UTF-8")
-public class ChatController {
-
-    @Autowired
-    private NettyServer server;
-
-    @PostMapping("/sendMsg")
-    public void sendMsg(@RequestBody Content content){
-        server.sendMsg(content);
-    }
-}
+//package com.example.chat.controller;
+//
+//import com.example.chat.netty.NettyServer;
+//import com.example.chat.netty.element.Content;
+//import com.example.chat.netty.handler.ChatHandler;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//@RequestMapping(value = "/chat", produces = "application/json;charset=UTF-8")
+//public class ChatController {
+//
+//    @Autowired
+//    private NettyServer server;
+//
+//    @PostMapping("/sendMsg")
+//    public void sendMsg(@RequestBody Content content){
+//        server.sendMsg(content);
+//    }
+//}
